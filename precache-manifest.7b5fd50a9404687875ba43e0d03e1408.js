e76d6b11b685f1a73df8bb558a79f24dc1e62f43
benny <bennytajfar@gmail.com>
2020-10-05 13:04:27 -0500

    Updates

---
+++ precache-manifest.7b5fd50a9404687875ba43e0d03e1408.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49752049cd68875a3ecc5a250fbe4360",
+    "url": "/bennyuniverse/index.html"
+  },
+  {
+    "revision": "74b601502275094d1ca5",
+    "url": "/bennyuniverse/static/css/main.bf3baa33.chunk.css"
+  },
+  {
+    "revision": "a26b1f73a2f0c3d5dbe5",
+    "url": "/bennyuniverse/static/js/2.2e68f8ae.chunk.js"
+  },
+  {
+    "revision": "5f8be6edcb631ea3c834b8459dc2c264",
+    "url": "/bennyuniverse/static/js/2.2e68f8ae.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "74b601502275094d1ca5",
+    "url": "/bennyuniverse/static/js/main.f8d13e37.chunk.js"
+  },
+  {
+    "revision": "afc9aa06ba3f5d49b7a8",
+    "url": "/bennyuniverse/static/js/runtime-main.c1e3df4e.js"
+  },
+  {
+    "revision": "d846540c7529e75816f2cf6c3b6fe71f",
+    "url": "/bennyuniverse/static/media/logo.d846540c.png"
+  }
+]);
